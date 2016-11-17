c241c7361fc8b8de3a33e62a1d504428703ceaac
Dr. Mike Hopper <drmikeh@gmail.com>
2016-11-17 14:30:03 -0500

    Improved code.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,9 +9,9 @@ let game = {
   currentPlayer: 'X',
 
   reset: function() {
-    for (var r = 0; r < this.board.length; r++) {
+    for (let r = 0; r < this.board.length; r++) {
       let row = this.board[r];
-      for (var c = 0; c < row.length; c++) {
+      for (let c = 0; c < row.length; c++) {
         row[c] = '?';
       }
     }
@@ -28,14 +28,10 @@ let game = {
     this.board[r][c] = this.currentPlayer;
   },
 
-  checkForMatch: function(cell1, cell2, cell3) {
-    return cell1 === cell2 && cell1 === cell3 && cell1 !== '?';
-  },
-
   isBoardFull: function() {
-    for (var r = 0; r < this.board.length; r++) {
+    for (let r = 0; r < this.board.length; r++) {
       let row = this.board[r];
-      for (var c = 0; c < row.length; c++) {
+      for (let c = 0; c < row.length; c++) {
         if (this.board[r][c] === '?') {
           return false;
         }
@@ -44,15 +40,38 @@ let game = {
     return true;
   },
 
+  checkForMatch: function(cell1, cell2, cell3) {
+    return cell1 === cell2 && cell1 === cell3 && cell1 !== '?';
+  },
+
+  checkRows: function() {
+    for (let r = 0; r < this.board.length; r++) {
+      let row = this.board[r];
+      if (this.checkForMatch(row[0], row[1], row[2])) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  checkColumns: function() {
+    for (let c = 0; c < this.board[0].length; c++) {
+      if (this.checkForMatch(this.board[0][c], this.board[1][c], this.board[2][c])) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  checkDiagonals: function() {
+    return this.checkForMatch(this.board[0][0], this.board[1][1], this.board[2][2])
+        || this.checkForMatch(this.board[2][0], this.board[1][1], this.board[0][2]);
+  },
+
   checkForEndOfGame: function() {
-    let rowMatch = this.checkForMatch(this.board[0][0], this.board[0][1], this.board[0][2])
-                || this.checkForMatch(this.board[1][0], this.board[1][1], this.board[1][2])
-                || this.checkForMatch(this.board[2][0], this.board[2][1], this.board[2][2]);
-    let colMatch = this.checkForMatch(this.board[0][0], this.board[1][0], this.board[2][0])
-                || this.checkForMatch(this.board[0][1], this.board[1][1], this.board[2][1])
-                || this.checkForMatch(this.board[0][2], this.board[1][2], this.board[2][2]);
-     let diagMatch = this.checkForMatch(this.board[0][0], this.board[1][1], this.board[2][2])
-                  || this.checkForMatch(this.board[2][0], this.board[1][1], this.board[0][2]);
+    let rowMatch = this.checkRows();
+    let colMatch = this.checkColumns();
+     let diagMatch = this.checkDiagonals();
      this.winner = rowMatch || colMatch || diagMatch;
      this.cat = !this.winner && this.isBoardFull();
      return this.winner || this.cat;
